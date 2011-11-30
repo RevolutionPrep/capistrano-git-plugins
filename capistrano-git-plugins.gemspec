277de198e5eabe4f6b9f79eb90f464b03992e40d
Ryan Moran <ryan.moran@gmail.com>
2011-11-29 17:26:36 -0800

    patching bundler rebuild

--- capistrano-git-plugins.gemspec
+++ capistrano-git-plugins.gemspec
@@ -5,11 +5,11 @@
 
 Gem::Specification.new do |s|
   s.name = %q{capistrano-git-plugins}
-  s.version = "0.0.1"
+  s.version = "0.0.2"
 
   s.required_rubygems_version = Gem::Requirement.new(">= 0") if s.respond_to? :required_rubygems_version=
   s.authors = [%q{Ryan Moran}]
-  s.date = %q{2011-11-21}
+  s.date = %q{2011-11-30}
   s.description = %q{Plugins to support capistrano-git deployments}
   s.email = %q{ryan.moran@gmail.com}
   s.extra_rdoc_files = [
--- lib/capistrano/git/plugins/bundler.rb
+++ lib/capistrano/git/plugins/bundler.rb
@@ -15,7 +15,9 @@ Capistrano::Configuration.instance(:must_exist).load do
       task :rebuild, :except => { :no_release => true } do
         puts "\n\n### REBUILDING BUNDLE!: Wiping the existing bundle and rebuilding from scratch...\n\n"
         run "sudo rm -rf #{deploy_to}/vendor/bundle"
-        bundle_gems
+        args = ["--deployment"]
+        args << "--without #{bundle_without.join(" ")}" unless bundle_without.empty?
+        run "cd #{deploy_to} && sudo -u #{sudo_user} #{ruby_bin_dir}/bundle install #{args.join(' ')}"
       end
     end
   end
