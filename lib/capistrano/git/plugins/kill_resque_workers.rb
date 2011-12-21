051590c9d0d1cb29f245686df3a1f1accbebb4e7
Ryan Moran <ryan.moran@gmail.com>
2011-12-20 19:38:29 -0800

    making sure that the kill resque workers script always exits successfully

--- lib/capistrano/git/plugins/kill_resque_workers.rb
+++ lib/capistrano/git/plugins/kill_resque_workers.rb
@@ -5,7 +5,7 @@ Capistrano::Configuration.instance(:must_exist).load do
     desc 'restart resque workers'
     task :restart, :roles=> :god do
       puts "\n\n### RESTART RESQUE WORKERS!: Killing the resque workers, God will resurrect them.\n\n"
-      run "sudo #{deploy_to}/config/god/kill_resque_workers.sh"
+      run "sudo #{deploy_to}/config/god/kill_resque_workers.sh; exit 0"
     end
   end
 end
