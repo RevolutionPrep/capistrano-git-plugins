e430363fe35fe1df6c419c205aacbd261cb3196f
Ryan Moran <ryan.moran@gmail.com>
2012-01-04 14:34:12 -0800

    deploy_reporter domain change

--- lib/capistrano/git/plugins/deploy_reporter.rb
+++ lib/capistrano/git/plugins/deploy_reporter.rb
@@ -1,15 +1,17 @@
 require 'net/http'
 
 Capistrano::Configuration.instance(:must_exist).load do
-  after 'deploy',            'deploy:report'
-  after 'deploy:migrations', 'deploy:report'
+  if configuration[:deploy_reporter_domain]
+    after 'deploy',            'deploy:report'
+    after 'deploy:migrations', 'deploy:report'
+  end
 
   namespace :deploy do
     desc "Report that the deployment was made"
     task :report, :except => { :no_release => true } do
       local_user = ENV['USER'] || ENV['USERNAME']
       puts "\n\n### REPORT DEPLOYMENT: Reporting deployment to deployment tracking system\n\n"
-      url = URI.parse('http://rpdeploy.heroku.com/deployments')
+      url = URI.parse("#{configuration[:deploy_reporter_domain]}/deployments")
       req = Net::HTTP::Post.new(url.path)
       req.set_form_data({ 'deployment[deployer]' => local_user, 'deployment[project_name]' => application }, ';')
       res = Net::HTTP.new(url.host, url.port).start {|http| http.request(req) }
