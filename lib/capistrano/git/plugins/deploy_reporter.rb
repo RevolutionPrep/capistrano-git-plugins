7c788db76dd7a21b89f13d78e054f4fa3a5b1247
Ryan Moran <ryan.moran@gmail.com>
2012-01-04 14:31:03 -0800

    updating deploy_reporter task domain

--- lib/capistrano/git/plugins/deploy_reporter.rb
+++ lib/capistrano/git/plugins/deploy_reporter.rb
@@ -9,7 +9,7 @@ Capistrano::Configuration.instance(:must_exist).load do
     task :report, :except => { :no_release => true } do
       local_user = ENV['USER'] || ENV['USERNAME']
       puts "\n\n### REPORT DEPLOYMENT: Reporting deployment to deployment tracking system\n\n"
-      url = URI.parse('http://fierce-samurai-448.heroku.com/deployments')
+      url = URI.parse('http://rpdeploy.heroku.com/deployments')
       req = Net::HTTP::Post.new(url.path)
       req.set_form_data({ 'deployment[deployer]' => local_user, 'deployment[project_name]' => application }, ';')
       res = Net::HTTP.new(url.host, url.port).start {|http| http.request(req) }
