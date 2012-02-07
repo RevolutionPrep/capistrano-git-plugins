f96aabaaed6e50796f571368d8ebf0ac5f70b681
Steve Lawson <steve.lawson@revolutionprep.com>
2012-02-07 13:21:09 -0800

    Do not fail is deploy_reporter_domain is not set

--- lib/capistrano/git/plugins/deploy_reporter.rb
+++ lib/capistrano/git/plugins/deploy_reporter.rb
@@ -1,7 +1,9 @@
 require 'net/http'
 
 Capistrano::Configuration.instance(:must_exist).load do
-  if deploy_reporter_domain
+  reporter_domain = fetch(:deploy_reporter_domain) if exists?(:deploy_reporter_domain)
+
+  if reporter_domain
     after 'deploy',            'deploy:report'
     after 'deploy:migrations', 'deploy:report'
   end
@@ -11,7 +13,7 @@ Capistrano::Configuration.instance(:must_exist).load do
     task :report, :except => { :no_release => true } do
       local_user = ENV['USER'] || ENV['USERNAME']
       puts "\n\n### REPORT DEPLOYMENT: Reporting deployment to deployment tracking system\n\n"
-      url = URI.parse("#{deploy_reporter_domain}/deployments")
+      url = URI.parse("#{reporter_domain}/deployments")
       req = Net::HTTP::Post.new(url.path)
       req.set_form_data({ 'deployment[deployer]' => local_user, 'deployment[project_name]' => application }, ';')
       res = Net::HTTP.new(url.host, url.port).start {|http| http.request(req) }
