bba2352e4c05942c1e6eff68193323aeff866766
Monica McArthur <monica@revolutionprep.com>
2012-02-06 17:06:10 -0800

    Typo

--- lib/capistrano/git/plugins/kill_resque_workers.rb
+++ lib/capistrano/git/plugins/kill_resque_workers.rb
@@ -5,7 +5,7 @@ Capistrano::Configuration.instance(:must_exist).load do
     desc 'restart resque workers'
     task :restart, :roles=> :god do
       puts "\n\n### RESTART RESQUE WORKERS!: Telling god to restart our resque workers.\n\n"
-      run "sudo #{ruby_bin_dir}/ruby #{ruby_bin_dir}god restart #{application-resque}; exit 0"
+      run "sudo #{ruby_bin_dir}/ruby #{ruby_bin_dir}god restart #{application}-resque; exit 0"
     end
   end
 end
