53500fa1c6e0079ac8853baa4d4766f5e12a5847
Steve Lawson <steve.lawson@revolutionprep.com>
2012-02-15 21:20:57 -0800

    Use :rake setting if provided

--- lib/capistrano/git/plugins/hoptoad.rb
+++ lib/capistrano/git/plugins/hoptoad.rb
@@ -7,7 +7,7 @@ Capistrano::Configuration.instance(:must_exist).load do
     task :notify_hoptoad, :except => { :no_release => true } do
       rails_env = fetch(:hoptoad_env, fetch(:rails_env, 'production'))
       local_user = ENV['USER'] || ENV['USERNAME']
-      executable = RUBY_PLATFORM.downcase.include?('mswin') ? 'rake.bat' : 'rake'
+      executable = fetch(:rake, (RUBY_PLATFORM.downcase.include?('mswin') ? 'rake.bat' : 'rake'))
       notify_command = "#{executable} hoptoad:deploy TO=#{rails_env} REPO=#{repository} USER=#{local_user}"
       notify_command << " API_KEY=#{ENV['API_KEY']}" if ENV['API_KEY']
       puts "\n\n### NOTIFY HOPTOAD: Notifying Hoptoad of Deploy (#{notify_command})\n\n"
