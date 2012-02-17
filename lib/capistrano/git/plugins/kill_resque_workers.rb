a8e4b20667b76ba2942e9199db43517ae9b06400
Steve Lawson <steve.lawson@revolutionprep.com>
2012-02-16 20:41:20 -0800

    Maybe it'll work with sudo...

--- lib/capistrano/git/plugins/kill_resque_workers.rb
+++ lib/capistrano/git/plugins/kill_resque_workers.rb
@@ -4,10 +4,8 @@ Capistrano::Configuration.instance(:must_exist).load do
   namespace :resque_workers do
     desc 'restart resque workers'
     task :restart, :roles=> :god do
-      puts "\n\n### STOP RESQUE WORKERS!: Telling god to stop our resque workers.\n\n"
-      run "sudo #{ruby_bin_dir}/ruby #{ruby_bin_dir}/god stop #{application}-resque; exit 0"
-      puts "\n\n### START RESQUE WORKERS!: Telling god to start our resque workers.\n\n"
-      run "sudo #{ruby_bin_dir}/ruby #{ruby_bin_dir}/god start #{application}-resque; exit 0"
+      puts "\n\n### RESTART RESQUE WORKERS!: Telling god to restart our resque workers.\n\n"
+      run "sudo -u #{sudo_user} #{ruby_bin_dir}/ruby #{ruby_bin_dir}/god restart #{application}-resque; exit 0"
     end
   end
 end
