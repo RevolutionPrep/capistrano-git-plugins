e344ec3326f5fefc2e2273b7b2ad9ccb0a1c76d0
Steve Lawson <smlsml@gmail.com>
2012-02-20 11:18:57 -0800

    Use current environment not production

--- lib/capistrano/git/plugins/hoptoad.rb
+++ lib/capistrano/git/plugins/hoptoad.rb
@@ -9,7 +9,7 @@ Capistrano::Configuration.instance(:must_exist).load do
       local_user = ENV['USER'] || ENV['USERNAME']
       executable = fetch(:rake, (RUBY_PLATFORM.downcase.include?('mswin') ? 'rake.bat' : 'rake'))
       executable = "cd #{deploy_to} && sudo #{executable}" if executable.include?('bundle exec')
-      notify_command = "#{executable} hoptoad:deploy TO=#{rails_env} REPO=#{repository} USER=#{local_user} RAILS_ENV=production"
+      notify_command = "#{executable} hoptoad:deploy TO=#{rails_env} REPO=#{repository} USER=#{local_user} RAILS_ENV=#{rails_env}"
       notify_command << " API_KEY=#{ENV['API_KEY']}" if ENV['API_KEY']
       puts "\n\n### NOTIFY HOPTOAD: Notifying Hoptoad of Deploy (#{notify_command})\n\n"
       run "#{notify_command}"
